620e5165f0140477379ef595208de7068fcdf899
KONDWANI_NGULUBE <kondwanigideon.ngulube@gmail.com>
2020-09-15 10:47:17 +0200

    cart-component

--- shoppingcart/src/App.js
+++ shoppingcart/src/App.js
@@ -3,17 +3,43 @@ import data from './data.json'
 import Products from './Components/products';
 import Filter from './Components/Filter';
 import products from './Components/products';
+import Cart from './Components/Cart';
+
 class  App extends React.Component{
   constructor(){
     super();
     this.state= {
       products: data.products,
+      cartItems:[],
       size:"",
       sort:""     
     }
-    this.filterProducts = this.filterProducts.bind(this)
-    this.sortProducts = this.sortProducts.bind(this)
+    this.filterProducts = this.filterProducts.bind(this);
+    this.sortProducts = this.sortProducts.bind(this);
   }
+
+  removeFromCart = (product)=> {
+    const cartItems= this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x)=>x._id != product._id)
+    });
+  }
+  
+
+  addToCart= (product) => {
+    const cartItems= this.state.cartItems.slice();
+    let alreadyInCart= false;
+    cartItems.forEach(item => {
+      if(item._id ===product._id){
+        item.count++;
+        alreadyInCart= true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count:1})
+    };
+    this.setState({cartItems});
+  } 
 
   filterProducts(event){
     console.log(event.target.value);
@@ -61,9 +87,16 @@ class  App extends React.Component{
               filterProducts={this.filterProducts}
               sortProducts={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products}></Products>
+              <Products 
+                products={this.state.products}
+                addToCart={this.addToCart}
+              ></Products>
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart 
+              cartItems={this.state.cartItems}
+              removeFromCart={this.removeFromCart}/>
+            </div>
           </div>
         </main>
         <footer>All rights reserved</footer>
--- shoppingcart/src/Components/products.js
+++ shoppingcart/src/Components/products.js
@@ -1,20 +1,23 @@
- import React, { Component } from 'react'
+import React, { Component } from 'react'
 import formatCurrency from './uti'
  
  export default class products extends Component {
+    
      render() {
          return (
              <div>
                  <ul className='products'>
-                     {this.props.products.map((products) =>(
-                         <li key={products._id}>
+                     {this.props.products.map((product) =>(
+                         <li key={product._id}>
                              <div className='product'>
                                  <a href={"#" + products._id}>
-                                     <img src={products.image} alt={products.title}></img>
+                                     <img src={product.image} alt={product.title}></img>
                                  </a>
                                  <p>{products.title}</p>
-                                 <div className="product-price">{ formatCurrency(products.price)}</div>
-                                 <button className="button primary">Add to Cart</button>
+                                 <div className="product-price">{ formatCurrency(product.price)}</div>
+                                 <button
+                                  onClick={()=> this.props.addToCart(product)}
+                                   className="button primary">Add to Cart</button>
                              </div>
                          </li>
                      ))}
